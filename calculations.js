f3b03e3a9ca096a39edafe6b5f377884de3cdcea
Lota Plezere <lota.plezere@gmail.com>
2016-10-31 12:12:37 +0200

    Rectangle and triangle formulas added, optimized functionality

--- calculations.js
+++ calculations.js
@@ -1,24 +1,52 @@
-// When user has put in the value(s) and pressed the "calculate" button, return the result of the area.
+  // Get figure type, sort by the method that is relevand to the specified figure
+function calculate(figureType) {
+  switch (figureType) {
 
+    case "circle":
+      // Get the radius value for circle from the textarea with id "radius"
+      var radius = document.getElementById("radius").value;
+      //Check if the number is above 0 and an actual number
+      if (validateNumber(radius)) {
+        // Display final result in a div
+        var circleResult = document.getElementById("circleArea");
+        circleResult.innerHTML = calculateCircleArea(radius);
+        // If value is not a number (or below zero), display an alert
+      } else {
+        notANumber();
+      }
+      break;
 
-function calculate() {
-  // Get user input
-  var radius = document.getElementById("radius").value;
+    case "rectangle":
+      var rectangleLength = document.getElementById("rect-length").value;
+      var rectangleWidth = document.getElementById("rect-width").value;
+      if (validateNumber(rectangleLength) && validateNumber(rectangleWidth)) {
+        var rectangleResult = document.getElementById("rectangleArea");
+        rectangleResult.innerHTML = calculateRectangleArea(rectangleLength, rectangleWidth);
+        // If value is not a number (or below zero), display an alert
+      } else {
+        notANumber();
+      }
+      break;
 
-  if (validateNumber(radius)) {
-    // Display final result
-    var circleResult = document.getElementById("circleArea");
-    circleResult.innerHTML = calculateCircleArea(radius);
-  } else {
-    alert("Hmm, something went wrong. Please make sure you enter a number above 0.")
+    case "triangle":
+      var triBase = document.getElementById("tri-base").value;
+      var triHeigth = document.getElementById("tri-heigth").value;
+      if (validateNumber(triBase) && validateNumber(triHeigth)) {
+        var triangleResult = document.getElementById("triangleArea");
+        triangleResult.innerHTML = calculateTriangleArea(triBase, triHeigth);
+        // If value is not a number (or below zero), display an alert
+      } else {
+      notANumber();
+      }
+      break;
+
   }
 }
 
 
-function validateNumber(radius) {
-  var inputValidator = isNaN(radius);
-
-  if (inputValidator == false && radius > 0) {
+function validateNumber(input) {
+  var inputValidator = isNaN(input);
+  if (inputValidator == false && input > 0) {
     return true;
   } else {
     return false;
@@ -30,4 +58,23 @@ function calculateCircleArea(radius) {
   var circleArea = Math.round(radius * 3.14);
   console.log("Circle area is: " + circleArea);
   return circleArea;
+}
+
+
+function calculateRectangleArea(rectangleLength, rectangleWidth) {
+  var rectangleArea = Math.round(rectangleLength * rectangleWidth);
+  console.log("Rectangle area is: " + rectangleArea);
+  return rectangleArea;
+}
+
+
+function calculateTriangleArea(triBase, triHeigth) {
+  var triangleArea = Math.round((triBase * triHeigth)/2);
+  console.log("Triangle area is: " + triangleArea);
+  return triangleArea;
+}
+
+
+function notANumber() {
+  alert("Hmm, something went wrong. Please make sure you enter a number above 0.");
 }
